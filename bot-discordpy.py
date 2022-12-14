fccd42a8ae12fe5585a6e9297eeddc53247d6b84
Zed <silyirishman1983@gmail.com>
2022-12-14 16:13:24 +0000

    wed14thdec

--- bot-discordpy.py
+++ bot-discordpy.py
@@ -20,7 +20,8 @@ async def on_ready():
             🍞🍞░░░██║░░░██║░░██║███████║╚█████╗░░░░██║░░░██████╦╝██║░░██║░░░██║░░░🍞🍞
             🍞🍞░░░██║░░░██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║░░░██║░░░🍞🍞
             🍞🍞░░░██║░░░╚█████╔╝██║░░██║██████╔╝░░░██║░░░██████╦╝╚█████╔╝░░░██║░░░🍞🍞
-            🍞🍞░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═════╝░░╚════╝░░░░╚═╝░░░🍞🍞''')
+            🍞🍞░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═════╝░░╚════╝░░░░╚═╝░░░🍞🍞
+            ''')
     
     try:
         synced = await bot.tree.sync()
@@ -29,13 +30,26 @@ async def on_ready():
         print(e)
         
 @bot.tree.command(name="ping", 
-                  description = 'Says "Pong!"')
+                  description = 'Says "Pong!" with the amount of time(ms) it took!')
 async def ping(Interaction: discord.Interaction):
-    await Interaction.response.send_message(f"Pong!")
+    await Interaction.response.send_message(f"Pong!\nThat took `{round(bot.latency * 1000)}ms` for me to respond!")
     
-@bot.tree.command(name="strange insults", 
-                  description = 'Says "Pon')
-async def ping(Interaction: discord.Interaction):
-    await Interaction.response.send_message(f"Pong!")
+@bot.tree.command(name="toast-insults", 
+                  description = 'Make ToastBot reply with a toast-related insult')
+async def toastinsults(Interaction: discord.Interaction):
+    my_file = open("strangeinsults.txt", "r",encoding="utf-8")
+    content_list = my_file.readlines()
+    await Interaction.response.send_message(str(content_list[random.randint(0,len(content_list)-1)]))
+    
+@bot.tree.command(name="time", 
+                  description = 'Says the current time (Currently in uk)')
+async def timecmd(Interaction: discord.Interaction):
+    t = time.localtime()
+    time_get = time.strftime("%I:%M %p", t)
+    time_hour = time.strftime("%I", t)
+    time_aftrm = time.strftime("%p", t)
+    await Interaction.response.send_message("The current time in the UK is: "+time_get)
+    if int(time_hour) >= 12 and time_aftrm == "PM":
+        await Interaction.response.send_message("<@592732403546587323>! GO TO SLEEP!")
     
 bot.run(TOKEN)
