10f85e0eb72974eaa24a728a092e13e8552c2908
BzZ7t <silyirishman1983@gmail.com>
2023-05-11 13:50:44 +0100

    update to mod.py

--- plugins/mod.py
+++ plugins/mod.py
@@ -14,7 +14,7 @@ async def mod(ctx):
 
 @mod.child
 @lightbulb.add_cooldown(3.0, 1, lightbulb.GuildBucket)
-@lightbulb.add_checks(lightbulb.owner_only)
+#@lightbulb.add_checks()
 @lightbulb.option("reason",
                   " (Optional) What is the reason for ban?")
 @lightbulb.option("user",
@@ -23,18 +23,16 @@ async def mod(ctx):
                    'ban a member from the server')
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def ban(ctx):
-    #TODO: make responses empheral..
-    if ctx.author.id == 592732403546587323:
-        server = ctx.get_guild() #TODO: fix user_id: - Value "<@USERID>" is not snowflake.
-        ban_reason = ctx.options.reason
-        remove_chara = ["<","@",">"]
-        user_interact = ctx.options.user
-        for x in remove_chara:
-            user_interact = str.replace(user_interact,x, "")
-        await server.ban(user_interact, reason=ban_reason)
-        await ctx.respond(f"{ctx.options.user} was succesfully banned with reason:\n{ban_reason}", '''ephemeral = True''')
-    else: #TODO: Remove if statment when code is finished
-        await ctx.respond("My creator doesn't trust this command yet, please try another time", '''ephemeral = True''')
+    #TODO: make responses empheral and add permission checks ffs..
+    server = ctx.get_guild() #TODO: fix user_id: - Value "<@USERID>" is not snowflake.
+    ban_reason = ctx.options.reason
+    remove_chara = ["<","@",">"]
+    user_interact = ctx.options.user
+    
+    for x in remove_chara:
+        user_interact = str.replace(user_interact,x, "")
+    await server.ban(user_interact, reason=ban_reason)
+    await ctx.respond(f"{ctx.options.user} was succesfully banned with reason:\n`{ban_reason}`", '''ephemeral = True''')
     
     
     
