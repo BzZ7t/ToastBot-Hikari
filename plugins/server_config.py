103d37c8d40b439a59afe3ae81d7c880ac8efb13
BzZ7t <silyirishman1983@gmail.com>
2023-07-02 00:37:19 +0100

    miru stuff works, channel perms don't still

--- plugins/server_config.py
+++ plugins/server_config.py
@@ -6,7 +6,6 @@ import hikari
 import lightbulb
 import miru
 
-
 plugin = lightbulb.Plugin('server_config')
 plugin.add_checks(lightbulb.guild_only)
 
@@ -29,12 +28,13 @@ goodbye_keys = ['goodbye_channel',
                 ]
 
 ticket_keys = ['ticket_channel',
+               'ticket_message',
                'ticket_txt',
                'ticket_create_txt',
                'ticket_num']
 
 misc_keys = ['server_name',
-             'mod_all']
+             'mod_role']
 
 # Common functions
 # Writes a dictionary to an existing/new json file
@@ -75,6 +75,11 @@ async def json_erase(ctx: lightbulb.Context, key_list):
     server = ctx.get_guild().id
     file_location = r"server_save/{server}.json".format(server=server)
     
+    if ctx.options.setting == 'ticket':
+        channel = await json_get(server,'ticket_channel')
+        message = await json_get(server,'ticket_message')
+        await ctx.bot.rest.delete_message(channel,message)
+
     try:
         with open(file_location,'r+', encoding="utf-8") as fs:
             json_file = json.load(fs)
@@ -92,6 +97,7 @@ async def json_erase(ctx: lightbulb.Context, key_list):
     except FileNotFoundError or json.decoder.JSONDecodeError or KeyError:
         pass
     
+        
 async def json_get(server, key):
     try:
         open(f'server_save/{server}.json', 'r', encoding='utf-8')
@@ -128,9 +134,12 @@ async def config_base(ctx:lightbulb.Context, type):
             f"{type}_txt":message}
     
     if type == 'welcome':
-        role = ctx.options.role
-        dict['welcome_role'] = role.id
-        
+        if ctx.options.role != None:
+            role = ctx.options.role
+            dict['welcome_role'] = role.id
+        else:
+            pass
+
     if type == 'ticket':
         creation_message = ctx.options.creation_message
         mod_role = ctx.options.mod_role
@@ -142,36 +151,89 @@ async def config_base(ctx:lightbulb.Context, type):
     await json_write(ctx,dict)
     channel = await json_get(server.id, f"{type}_channel")
     message = await json_get(server.id, f"{type}_txt")
-    if isinstance(message, list) and 'ticket':
+    if isinstance(message, list):
         message = random.choice(message)
 
     await ctx.edit_last_response(f"{type} channel has successfully been set to <#{channel}>\n{message.format(member=member, server=server.name, count=server.member_count)}", 
                                  user_mentions=False)
 
+async def ticket_create(ctx: lightbulb.Context):
+    server = ctx.get_guild()
+    member = ctx.author
+    mod = await json_get(server.id,"mod_role")
+    message = await json_get(server.id,"ticket_create_txt")
+    number = await json_get(server.id, "ticket_num")
+    number =+ 1
+    channel_name = f'Ticket #{number}'
+    
+    channel_id = await ctx.get_channel().app.rest.create_guild_text_channel(server.id,
+                                                                            channel_name,
+                                                                            )
+    
+    # Very strange issue with edit_channel(), Error leads to following:
+    
+    # File "/home/mint/.local/lib/python3.10/site-packages/hikari/internal/data_binding.py", line 356, in put_array
+    # self[key] = [conversion(value) for value in values]
+    # TypeError: 'PermissionOverwrite' object is not iterable
+    
+    # TODO: Possible Hikari bug, if not, figure  out what you did wrong
+    
+    await ctx.bot.rest.edit_channel(channel_id,
+                                    permission_overwrites=hikari.PermissionOverwrite(
+                                        id=server.id,
+                                        type=hikari.PermissionOverwriteType.ROLE,
+                                        deny=(
+                                            hikari.Permissions.VIEW_CHANNEL
+                                            | hikari.Permissions.SEND_MESSAGES
+                                            | hikari.Permissions.READ_MESSAGE_HISTORY
+                                            )
+                                        ),
+                                    )
+    
+    await ctx.bot.rest.edit_channel(channel_id, 
+                                    permission_overwrites=hikari.PermissionOverwrite(
+                                        id=member,
+                                        type=hikari.PermissionOverwriteType.MEMBER,
+                                        allow=(
+                                            hikari.Permissions.VIEW_CHANNEL
+                                            | hikari.Permissions.SEND_MESSAGES
+                                            | hikari.Permissions.READ_MESSAGE_HISTORY
+                                            )
+                                        )
+                                    )
+    
+    await ctx.bot.rest.edit_channel(channel_id, 
+                                    permission_overwrites=hikari.PermissionOverwrite(
+                                        id=mod,
+                                        type=hikari.PermissionOverwriteType.ROLE,
+                                        allow=(
+                                            hikari.Permissions.VIEW_CHANNEL
+                                            | hikari.Permissions.SEND_MESSAGES
+                                            | hikari.Permissions.READ_MESSAGE_HISTORY
+                                            )
+                                        )
+                                    )
+    
+    await ctx.bot.rest.create_message(channel_id,message.format(member=member.mention, mod=f'<@&{mod}>'),
+                                    user_mentions=True,role_mentions=True)
+    await json_write(ctx,{"ticket_num": number})
+
 # Common classes
-class tickets(miru.View):
+class tickets_system(miru.View):
     def __init__(self) -> None:
         super().__init__(timeout=None)  # Setting timeout to None
 
     @miru.button(label='Create a Ticket', style=hikari.ButtonStyle.PRIMARY)
     async def btn_create_ticket(self, button: miru.Button, ctx: miru.ViewContext) -> None:
-        server = ctx.get_guild()
-        member = ctx.author
-        channel = await json_get(server.id,"ticket_channel")
-        message = await json_get(server.id,"ticket_create_txt")
-        number = await json_get(server.id, "ticket_num")
-        number =+ 1
-        
-        await json_write(ctx,{"ticket_num": number})
-        await ctx.bot.rest.create_message_thread(channel, message.format(member=member.mention), f'Ticket #{number}',)
+        await ticket_create(ctx)
         
 
-# Listeners 
+# Listeners
 # Listens to a server name change TODO: unsure is this is its only function, docs says "Event fired when an existing guild is updated" which is so vague
 @plugin.listener(hikari.StartedEvent)
 async def persistant_miru(event: hikari.StartedEvent):
-    #view = tickets()
-    #await view.start()
+    view = tickets_system()
+    await view.start()
     pass
 
 @plugin.listener(hikari.GuildUpdateEvent)
@@ -234,16 +296,16 @@ async def welcome_join(event: hikari.MemberDeleteEvent) -> None:
 @lightbulb.option('setting',
                   'choose a setting to reset',
                   required=True,
-                  choices=[hikari.CommandChoice(name='welcome',value=welcome_keys),
-                           hikari.CommandChoice(name='goodbye',value=goodbye_keys),
-                           hikari.CommandChoice(name='tickets',value=ticket_keys),])
+                  choices=[hikari.CommandChoice(name='welcome',value='welcome'),
+                           hikari.CommandChoice(name='goodbye',value='goodbye'),
+                           hikari.CommandChoice(name='tickets',value='ticket'),])
 @lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
 @lightbulb.command('reset',
                    'reset server settings')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def reset(ctx: lightbulb.Context):
     setting = ctx.options.setting
-    erase_list = ctx.options.setting
+    erase_list = globals()[f'{setting}_keys']
     
     await json_erase(ctx, erase_list)
     await ctx.respond(f'{setting} setting has been reset')
@@ -280,6 +342,8 @@ async def welcome(ctx: lightbulb.Context):
         message = await json_get(server.id, 'welcome_txt')
     
         await json_write(ctx,{"welcome_role": role.id})
+        if isinstance(message, list):
+            message = random.choice(message)
         await ctx.edit_last_response(f"Welcome channel has successfully been set to <#{channel}>\nUser will join with role: <@&{role}>\n{message.format(member=member, server=server.name, count=server.member_count)}", 
                                  user_mentions=False, role_mentions=False)
         
@@ -299,7 +363,7 @@ async def welcome(ctx: lightbulb.Context):
                    'set up a goodbye channel') 
 @lightbulb.implements(lightbulb.SlashCommand)
 async def goodbye(ctx: lightbulb.Context):
-    await config_base(ctx, 'welcome')
+    await config_base(ctx, 'goodbye')
 
 @plugin.command
 @lightbulb.option('creation_message',
@@ -319,7 +383,8 @@ async def goodbye(ctx: lightbulb.Context):
                   hikari.TextableGuildChannel,
                   required=True)
 @lightbulb.command('tickets',
-                   'set up a tickets channel', auto_defer=True)
+                   'set up a tickets channel', 
+                   auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def tickets(ctx: lightbulb.Context):
     await config_base(ctx,'ticket')
@@ -333,76 +398,20 @@ async def tickets(ctx: lightbulb.Context):
     await ctx.edit_last_response(f"Ticket channel has successfully been set to <#{channel}>\n{message}\n\n{creation_message.format(member=member.mention,mod=f'<@&{mod}>')}", 
                                  user_mentions=False, role_mentions=False)
     
-    #view = tickets()
-    #message = await ctx.bot.rest.create_message(channel,message, components=view)
-    # await view.start(message)  #TODO: why does this not fucking work???
-    await ctx.respond('for now, a user will have to use /open-ticket to open a ticket, channel is currently unused', flags=hikari.MessageFlag.EPHEMERAL)
+    view = tickets_system()
+    message = await ctx.bot.rest.create_message(channel,message, components=view)
+    await view.start(message)  #TODO: why does this not fucking work???
+    await json_write(ctx,{'ticket_message': message.id})
+    
 
 @plugin.command
 @lightbulb.command('open-ticket',
-                   'open a ticket')
+                   'open a ticket',
+                   auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def openticket(ctx: lightbulb.Context):
-    server = ctx.get_guild()
-    member = ctx.author
-    mod = await json_get(server.id,"mod_role")
-    message = await json_get(server.id,"ticket_create_txt")
-    number = await json_get(server.id, "ticket_num")
-    number =+ 1
-    channel_name = f'Ticket #{number}'
-    
     await ctx.respond("You'll be mentioned once your ticket is created", flags=hikari.MessageFlag.EPHEMERAL)
-    channel_id = await ctx.get_channel().app.rest.create_guild_text_channel(server.id,
-                                                                            channel_name,
-                                                                            )
-    
-    # Very strange issue with edit_channel(), Error leads to following:
-     
-    # File "/home/mint/.local/lib/python3.10/site-packages/hikari/internal/data_binding.py", line 356, in put_array
-    # self[key] = [conversion(value) for value in values]
-    # TypeError: 'PermissionOverwrite' object is not iterable
-    
-    # TODO: Possible Hikari bug, if not, figure  out what you did wrong
-    
-    await ctx.bot.rest.edit_channel(channel_id,
-                                    permission_overwrites=hikari.PermissionOverwrite(
-                                        id=server.id,
-                                        type=hikari.PermissionOverwriteType.ROLE,
-                                        deny=(
-                                            hikari.Permissions.VIEW_CHANNEL
-                                            | hikari.Permissions.SEND_MESSAGES
-                                            | hikari.Permissions.READ_MESSAGE_HISTORY
-                                            )
-                                        )
-                                    )
-    
-    await ctx.bot.rest.edit_channel(channel_id, 
-                                    permission_overwrites=hikari.PermissionOverwrite(
-                                        id=member,
-                                        type=hikari.PermissionOverwriteType.MEMBER,
-                                        allow=(
-                                            hikari.Permissions.VIEW_CHANNEL
-                                            | hikari.Permissions.SEND_MESSAGES
-                                            | hikari.Permissions.READ_MESSAGE_HISTORY
-                                            )
-                                        )
-                                    )
-    
-    await ctx.bot.rest.edit_channel(channel_id, 
-                                    permission_overwrites=hikari.PermissionOverwrite(
-                                        id=mod,
-                                        type=hikari.PermissionOverwriteType.ROLE,
-                                        allow=(
-                                            hikari.Permissions.VIEW_CHANNEL
-                                            | hikari.Permissions.SEND_MESSAGES
-                                            | hikari.Permissions.READ_MESSAGE_HISTORY
-                                            )
-                                        )
-                                    )
-    
-    await ctx.bot.rest.create_message(channel_id,message.format(member=member.mention, mod='<@&{mod}>'),
-                                      user_mentions=True,role_mentions=True)
-    await json_write(ctx,{"ticket_num": number})
+    await ticket_create()
     
 # Loads the plugin
 def load(bot):
